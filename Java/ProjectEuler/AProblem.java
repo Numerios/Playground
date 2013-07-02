4e03b49c854a6b8c985cabda6397b5e9ddcdc2c8
Numerios <mrnumerios@gmail.com>
2013-07-02 18:41:33 +0200

    Added an abstract "template" for other problem classes

---
+++ Java/ProjectEuler/AProblem.java
@@ -0,0 +1,12 @@
+/* Playground - Java - Project Euler
+ *  
+ * Problem xxx
+ * http://projecteuler.net/problem=xxx
+ * 
+ * "description"
+ */
+public abstract class AProblem {
+	public static void main(String[] args) {
+
+	}
+}
--- Java/ProjectEuler/Problem001.java
+++ Java/ProjectEuler/Problem001.java
@@ -6,7 +6,7 @@
  * "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
  *  Find the sum of all the multiples of 3 or 5 below 1000."
  */
-public class Problem001 {
+public class Problem001 extends AProblem{
 	public static int RANGE = 1000;
 	public static int[] MULTIPLIERS = { 3, 5 };
 
