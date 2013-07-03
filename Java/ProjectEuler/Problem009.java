37f63d1f47928b73aa95279249114cc069266017
Numerios <mrnumerios@gmail.com>
2013-07-03 15:22:03 +0200

    Problem 009 now extends AProblem aswell.

--- Java/ProjectEuler/Problem009.java
+++ Java/ProjectEuler/Problem009.java
@@ -6,7 +6,7 @@
  * "A Pythagorean triplet is a set of three natural numbers, a  b  c, for which, a^2 + b^2 = c^2.
  *  There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc."
  */
-public class Problem009 {
+public class Problem009 extends AProblem{
 	// A bit brute-forcey
 	public static int GOAL = 1000; // the number we're trying to get to
 
