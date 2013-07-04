d99b85427138c6def1adc14fe2205075230a7663
Numerios <mrnumerios@gmail.com>
2013-07-05 00:12:39 +0200

    The bot can now ping + a few other reactions

--- Java/IRCbot/Grammar.java
+++ Java/IRCbot/Grammar.java
@@ -2,24 +2,21 @@ import java.io.IOException;
 import java.util.Random;
 
 public class Grammar {
-	public static String[] grammarCheck = { "aby jsi", "aby jsme", "aby jste",
-			"abysme", "aby sme", "by jste", "by jsi", "hrupka", "hrupki", "hrubki" };
-	public static String[] grammarAnswer = { "abys", "abychom", "abyste",
-			"aby jsme", "aby jsme", "byste", "bys", "hrubka", "hrubky", "hrubky" };
-	public static String[] comment = { "Doporucoval bych se naucit cesky.",
-			"Kdo te sakra ucil cesky? ",
-			"Bro, do you even Czech your grammar? ",
-			"Vypni kompa a jdi se ucit cesky!", "Wow. Takova hrubka...",
-			"Kdo to po tobe ma sakra cist?", "OH GOD MY EYES" };
+    public static String[] grammarCheck = { "aby jsi", "aby jsme", "aby jste", "abysme", "aby sme", "by jste",
+            "by jsi", "hrupka", "hrupki", "hrubki" };
+    public static String[] grammarAnswer = { "abys", "abychom", "abyste", "aby jsme", "aby jsme", "byste", "bys",
+            "hrubka", "hrubky", "hrubky" };
+    public static String[] comment = { "Doporucoval bych se naucit cesky.", "Kdo te sakra ucil cesky? ",
+            "Bro, do you even Czech your grammar? ", "Vypni kompa a jdi se ucit cesky!", "Wow. Takova hrubka...",
+            "Kdo to po tobe ma sakra cist?", "OH GOD MY EYES" };
 
-	public static void checkGrammar(String channel) throws IOException {
-		for (int i = 0; i < grammarCheck.length; i++) {
-			if (IRCbot.buffer.toLowerCase().contains(grammarCheck[i].toLowerCase())) {
-				Random random = new Random();
-				IRCbot.say(channel, "* " + grammarAnswer[i] + "; "
-						+ comment[random.nextInt(comment.length)]);
-				break;
-			}
-		}
-	}
+    public static void checkGrammar(String channel) throws IOException {
+        for (int i = 0; i < grammarCheck.length; i++) {
+            if (IRCbot.buffer.toLowerCase().contains(grammarCheck[i].toLowerCase())) {
+                Random random = new Random();
+                IRCbot.say(channel, "* " + grammarAnswer[i] + "; " + comment[random.nextInt(comment.length)]);
+                break;
+            }
+        }
+    }
 }
--- Java/IRCbot/IRCbot.java
+++ Java/IRCbot/IRCbot.java
@@ -7,79 +7,93 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class IRCbot {
-	private static String SERVER = "efnet.xs4all.nl";
-	private static int PORT = 6667;
-	private static String NICK = "NumiBot";
+    private static String SERVER = "efnet.xs4all.nl";
+    private static int PORT = 6667;
+    private static String NICK = "NumiBot";
 
-	public static ArrayList<String> channelList = new ArrayList<String>();
+    public static ArrayList<String> channelList = new ArrayList<String>();
 
-	private static boolean running = false;
-	private static Socket socket;
-	private static BufferedReader in;
-	private static BufferedWriter out;
-	
-	public static String buffer;
-	/**
-	 * A stupid little bot I created in a few hours.
-	 * TODO: Responding to the exact person, using regex to search for stuff;
-	 */
-	public static void main(String[] args) throws IOException {
-		socket = new Socket(SERVER, PORT);
-		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		out = new BufferedWriter(new OutputStreamWriter(
-				socket.getOutputStream()));
-		if (socket.isConnected()) {
-			login(NICK);
+    private static boolean running = false;
+    private static Socket socket;
+    private static BufferedReader in;
+    private static BufferedWriter out;
 
-			channelList.add("technicraft.game");
-			join(channelList);
+    public static String buffer;
 
-			running = true;
-			new Thread().start();
-		}
-		while (running) {
-			try {
-				while ((buffer = in.readLine()) != null) {
-					respond(buffer, channelList.get(0), "\'o", "Nazdar! :)");
-					respond(buffer, channelList.get(0), "ahoj", "Nazdar! :)");
-					respond(buffer, channelList.get(0), "zdravim", "Nazdar! :)");
-					respond(buffer, channelList.get(0), "zdar", "Nazdar! :)");
-					respond(buffer, channelList.get(0), "helou", "Nazdar! :)");
-					Grammar.checkGrammar(channelList.get(0));
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    /**
+     * A stupid little bot I created in a few hours. 
+     * TODO: Responding to the exact person using regex to search for stuff;
+     */
+    public static void main(String[] args) throws IOException {
+        socket = new Socket(SERVER, PORT);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        if (socket.isConnected()) {
+            login(NICK);
 
-	public static void login(String nick) throws IOException {
-		out.write("USER Q Q Q :Q\n".replaceAll("Q", nick));
-		out.write("NICK " + nick + "\n");
-		out.flush();
-	}
+            channelList.add("technicraft.game");
+            join(channelList);
 
-	public static void join(ArrayList<String> chanList) throws IOException {
-		for (String channel : chanList) {
-			out.write("JOIN #" + channel + "\n");
-		}
-		out.flush();
-	}
+            running = true;
+            new Thread().start();
+        }
+        while (running) {
+            try {
+                while ((buffer = in.readLine()) != null) {
+                    System.out.println(buffer);
 
-	public static void say(String channel, String msg) throws IOException {
-		out.write("PRIVMSG #" + channel + " :" + msg + "\n");
-		out.flush();
-	}
-	
-	public static void respond(String b, String channel, String event, String response) throws IOException {
-		if (b.toLowerCase().contains(event.toLowerCase())) {
-			say(channel, response);
-		}
-	}
-	
-	public static void quit(String channel) throws IOException {
-		out.write("Ja jeste nechci odejit :(");
-		out.write("QUIT #" + channel + "\n");
-		out.flush();
-	}
+                    ping = System.currentTimeMillis();
+
+                    respond(buffer, channelList.get(0), "\\o", "Nazdar! :)");
+                    respond(buffer, channelList.get(0), "ahoj", "Nazdar! :)");
+                    respond(buffer, channelList.get(0), "zdravim", "Nazdar! :)");
+                    respond(buffer, channelList.get(0), "zdar", "Nazdar! :)");
+                    respond(buffer, channelList.get(0), "helou", "Nazdar! :)");
+                    respond(buffer, channelList.get(0), ">:)", "Y U EVIL?");
+                    respond(buffer, channelList.get(0), "nume?", "Eh?");
+                    respond(buffer, channelList.get(0), "ain't nobody got time", "No time? Bull$#it.");
+                    respond(buffer, channelList.get(0), "let the betatest begin", "Indeed.");
+                    respond(buffer, channelList.get(0), "da fug?", "* Toccata a fuga - J. S. Bach");
+
+                    Grammar.checkGrammar(channelList.get(0));
+
+                    if (buffer.startsWith("PING")) {
+                        out.write("PONG :" + buffer.substring(6).getBytes());
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void login(String nick) throws IOException {
+        out.write("USER Q Q Q :Q\n".replaceAll("Q", nick));
+        out.write("NICK " + nick + "\n");
+        out.flush();
+    }
+
+    public static void join(ArrayList<String> chanList) throws IOException {
+        for (String channel : chanList) {
+            out.write("JOIN #" + channel + "\n");
+        }
+        out.flush();
+    }
+
+    public static void say(String channel, String msg) throws IOException {
+        out.write("PRIVMSG #" + channel + " :" + msg + "\n");
+        out.flush();
+    }
+
+    public static void respond(String b, String channel, String event, String response) throws IOException {
+        if (b.toLowerCase().contains(event.toLowerCase())) {
+            say(channel, response);
+        }
+    }
+
+    public static void quit(String channel) throws IOException {
+        out.write("Ja jeste nechci odejit :(");
+        out.write("QUIT #" + channel + "\n");
+        out.flush();
+    }
 }
